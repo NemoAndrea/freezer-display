f56fdd5ce0560ffe2d85a0a94f40a03b0135604d
Nemo Andrea <nemoandrea@gmail.com>
2024-04-04 23:10:19 +0200

    Add simple script to simplifiy management of code
    Since we cannot instantiate git on the microcontroller drive, we simply copy the code over to the location of the repository on your main system.

---
+++ utils/copy-code-from-board.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# this code depends on being run from the root of the git repository. On windows you
+# can run it from git bash.
+#
+# If you prefer, you can of course always manually copy the file to the repository.
+#
+# change the path below for your particulary system configuration
+# the code below is for Windows and assumes the circuitpython is mounted at E:
+cp /e/code.py code.py
